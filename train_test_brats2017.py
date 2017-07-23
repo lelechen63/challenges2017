df149a4da80a284183a1943f9b35dc6f1ed82c3f
lchen63 <lchen63@cs.rochester.edu>
2017-07-23 00:57:11 -0400

    g

--- train_test_brats2017.py
+++ train_test_brats2017.py
@@ -29,7 +29,7 @@ def parse_inputs():
     parser.add_argument('-d', '--dense-size', dest='dense_size', type=int, default=256)
     parser.add_argument('-D', '--down-factor', dest='dfactor', type=int, default=500)
     parser.add_argument('-n', '--num-filters', action='store', dest='n_filters', nargs='+', type=int, default=[32])
-    parser.add_argument('-e', '--epochs', action='store', dest='epochs', type=int, default=4)
+    parser.add_argument('-e', '--epochs', action='store', dest='epochs', type=int, default=10)
     parser.add_argument('-q', '--queue', action='store', dest='queue', type=int, default=10)
     parser.add_argument('-u', '--unbalanced', action='store_false', dest='balanced', default=True)
     parser.add_argument('-s', '--sequential', action='store_true', dest='sequential', default=False)
@@ -257,9 +257,9 @@ def main():
 
                 net = Model(inputs=merged_inputs, outputs=[tumor, core, enhancing])
 
-                
-            net_name_before =  os.path.join(path,'baseline-brats2017.fold0.D500.f.p13.c3c3c3c3c3.n32n32n32n32n32.d256.e1.pad_valid.mdl')
-            net = keras.models.load_model(net_name_before)
+
+            # net_name_before =  os.path.join(path,'baseline-brats2017.fold0.D500.f.p13.c3c3c3c3c3.n32n32n32n32n32.d256.e1.pad_valid.mdl')
+            # net = keras.models.load_model(net_name_before)
             net.compile(optimizer='adadelta', loss='categorical_crossentropy', metrics=['accuracy'])
 
             print(c['c'] + '[' + strftime("%H:%M:%S") + ']    ' +

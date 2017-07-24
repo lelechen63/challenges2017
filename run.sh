9c6b75bb26fbbfc40a277902a99ed951fc144a24
lelechen63 <lchen63@ur.rochester.edu>
2017-07-24 11:03:59 -0400

    h

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-python train_test_brats2017.py -f /media/lele/DATA/brain/Brats17TrainingData/HGG/
+#python train_test_brats2017.py -f /mnt/disk0/dat/lchen63/brain/data/Brats17TrainingData/HGG/
+
+
+python train_test_2.py -f /mnt/disk0/dat/lchen63/brain/newdata/Brats17TrainingData/HGG2/

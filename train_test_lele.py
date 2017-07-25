fed36e242d15a51f9af9b8186dd3e8285913eb08
lchen63 <lchen63@cs.rochester.edu>
2017-07-25 15:13:09 -0400

    g

--- train_test_lele.py
+++ train_test_lele.py
@@ -21,7 +21,7 @@ def parse_inputs():
     # I decided to separate this function, for easier acces to the command line parameters
     parser = argparse.ArgumentParser(description='Test different nets with 3D data.')
     parser.add_argument('-f', '--folder', dest='dir_name', default='/home/mariano/DATA/Brats17CBICA/')
-    parser.add_argument('-F', '--n-fold', dest='folds', type=int, default=1)
+    parser.add_argument('-F', '--n-fold', dest='folds', type=int, default=5)
     parser.add_argument('-i', '--patch-width', dest='patch_width', type=int, default=13)
     parser.add_argument('-k', '--kernel-size', dest='conv_width', nargs='+', type=int, default=3)
     parser.add_argument('-c', '--conv-blocks', dest='conv_blocks', type=int, default=5)

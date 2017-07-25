1352975c0f4a9bce3bd86d9711a81ae89c3b3c04
lchen63 <lchen63@cs.rochester.edu>
2017-07-24 21:42:09 -0400

    f

--- train_test_lele.py
+++ train_test_lele.py
@@ -206,6 +206,7 @@ def main():
 
             # net_name_before =  os.path.join(path,'baseline-brats2017.fold0.D500.f.p13.c3c3c3c3c3.n32n32n32n32n32.d256.e1.pad_valid.mdl')
             # net = keras.models.load_model(net_name_before)
+            print net.summary()
             net.compile(optimizer='adadelta', loss='categorical_crossentropy', metrics=['accuracy'])
 
             print(c['c'] + '[' + strftime("%H:%M:%S") + ']    ' +

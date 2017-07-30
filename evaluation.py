f35cd0b4552b186c7b7a96f231b14c62a70f0365
lchen63 <lchen63@cs.rochester.edu>
2017-07-30 19:25:21 -0400

    g

--- evaluation.py
+++ evaluation.py
@@ -212,8 +212,8 @@ def pixel_level_evaluation(path = '/media/lele/DATA/brain/Brats17TrainingData/HG
         gt_path = patient
         seg_path = patient
         for f in fs:
-            if f[-10:-7] == 'est' and 'e4' in f:
-                seg_path = (patient + f)
+            if f[-10:-7] == 'est' and '.e4.' in f:
+                seg_path = patient + f
 
             elif f[-10:-7] =='seg':
                 gt_path = gt_path + f

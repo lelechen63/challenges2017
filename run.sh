04f75c9ec0e289c9172279206530c476e3cfa16f
lchen63 <lchen63@cs.rochester.edu>
2017-07-30 19:11:12 -0400

    g

--- run.sh
+++ run.sh
@@ -3,14 +3,14 @@
 
 #python train_test_5.py -f /media/lele/DATA/brain/Brats17TrainingData/HGG5/
 #python evaluation.py -p /media/lele/DATA/brain/Brats17TrainingData/HGG5/
-python evaluation.py -p /mnt/disk1/dat/brain/newdata/Brats17TrainingDa    ta/HGG4/
-#python train_test_lele.py -f /mnt/disk1/dat/brain/newdata/Brats17TrainingDat    a/HGG3/
+python evaluation.py -p /mnt/disk1/dat/brain/newdata/Brats17TrainingData/HGG4/
+#python train_test_lele.py -f /mnt/disk1/dat/brain/newdata/Brats17TrainingData/HGG3/
 
-#python train_test_brats2017.py -f /mnt/disk1/dat/brain/data/Brats17Training    Data/HGG/
+#python train_test_brats2017.py -f /mnt/disk1/dat/brain/data/Brats17TrainingData/HGG/
 
-#python train_test_iseg.py -f /mnt/disk1/dat/brain/newdata/Brats17TrainingDa    ta/HGG4/
+#python train_test_iseg.py -f /mnt/disk1/dat/brain/newdata/Brats17TrainingData/HGG4/
 
-#python train_test_2.py -f /mnt/disk1/dat/brain/newdata/Brats17TrainingData/    HGG2/
+#python train_test_2.py -f /mnt/disk1/dat/brain/newdata/Brats17TrainingData/HGG2/
 
 
 

8e641ec1f336db7d385ca5e5dbe1b0ab2e38970d
lchen63 <lchen63@cs.rochester.edu>
2017-08-01 16:53:03 -0400

    g

--- run.sh
+++ run.sh
@@ -1,17 +1,39 @@
 #python train_test_brats2017.py -f /mnt/disk0/dat/lchen63/brain/data/Brats17TrainingData/HGG/
+ 
 
+ #python train_test_5.py -f /media/lele/DATA/brain/Brats17TrainingData/HGG5/
+ 
+  
+ #python evaluation.py -p /media/lele/DATA/brain/Brats17TrainingData/HGG5/
+  
+ 
+  
+ #python evaluation.py -p /mnt/disk1/dat/brain/newdata/Brats17TrainingData/HGG2/
+ 
+  
+ #python evaluation.py -p /mnt/disk1/dat/brain/newdata/Brats17TrainingData/HGG3/
+ 
+ 
+ #python evaluation.py -p /mnt/disk1/dat/brain/newdata/Brats17TrainingData/HGG4/
 
-#python train_test_5.py -f /media/lele/DATA/brain/Brats17TrainingData/HGG5/
-#python evaluation.py -p /media/lele/DATA/brain/Brats17TrainingData/HGG5/
-#python evaluation.py -p /mnt/disk1/dat/brain/newdata/Brats17TrainingData/HGG4/
-#python train_test_lele.py -f /mnt/disk1/dat/brain/newdata/Brats17TrainingData/HGG3/
+ #python train_test_lele.py -f /mnt/disk1/dat/brain/newdata/Brats17TrainingData/HGG3/
+  
+ #python train_test_brats2017.py -f /mnt/disk1/dat/brain/newdata/Brats17TrainingData/HGG2/
+  
+ #python train_test_iseg.py -f /mnt/disk1/dat/brain/newdata/Brats17TrainingData/HGG4/
+ 
 
-#python train_test_brats2017.py -f /mnt/disk1/dat/brain/data/Brats17TrainingData/HGG/
-
-#python train_test_iseg.py -f /mnt/disk1/dat/brain/newdata/Brats17TrainingData/HGG4/
+#python train_test_model1_5folder.py -f  /mnt/disk1/dat/brain/newdata/Brats17TrainingData/HGG7/
+ 
+ 
+#python train_test_model2_5folder.py -f  /mnt/disk1/dat/brain/newdata/Brats17TrainingData/HGG8/
+ 
+ 
+#python train_test_model3_5folder.py -f  /mnt/disk1/dat/brain/newdata/Brats17TrainingData/HGG9/
+ 
+#python train_test_model4_5folder.py -f  /mnt/disk1/dat/brain/newdata/Brats17TrainingData/HGG10/
 
 #python train_test_2.py -f /mnt/disk1/dat/brain/newdata/Brats17TrainingData/HGG2/
-python train_test_model5_5folder.py -f /mnt/disk1/dat/brain/newdata/Brats17TrainingData/HGG11/
+#python train_test_model5_5folder.py -f /mnt/disk1/dat/brain/newdata/Brats17TrainingData/HGG11/
 
-
-
+python evaluation.py -p /mnt/disk1/dat/brain/newdata/Brats17TrainingData/HGG7/

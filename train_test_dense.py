784c5bbc986fc87a755c5a8ba4a73155ef74423a
lchen63 <lchen63@cs.rochester.edu>
2017-08-02 21:48:01 -0400

    g

--- train_test_dense.py
+++ train_test_dense.py
@@ -15,8 +15,7 @@ from data_creation import load_patch_batch_generator_test
 from data_manipulation.generate_features import get_mask_voxels
 from data_manipulation.metrics import dsc_seg
 from keras.layers.core import Dense, Dropout, Activation, Reshape
-from keras.layers.convolutional import Conv3D, UpSampling3D
-from keras_contrib.layers.convolutional import  Conv3DTranspose
+from keras.layers.convolutional import Conv3D, UpSampling3D, Conv3DTranspose
 from keras.layers.pooling import AveragePooling3D
 from keras.layers.pooling import GlobalAveragePooling3D
 from keras.layers import Input
